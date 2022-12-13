6a4b609947a797b0f62bb28b800e566515eeac31
Romulo Vieira <romulo.vieira777@gmail.com>
2022-12-13 20:10:42 -0300

    Introdução e Conceitos Básicos

--- Secao_01_Introducao_e_Conceitos_Basicos/simplemooc/simplemooc/core/views.py
+++ Secao_01_Introducao_e_Conceitos_Basicos/simplemooc/simplemooc/core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def home(request):
-    return render(request, 'home.html', {'usuario': 'Fulano de Tal'})
+    return render(request, 'home.html')
 
+def contact(request):
+    return render(request, 'contact.html')

b96de1c2c3bc60564babc393140e24dc1f6630a9
Romulo Vieira <romulo.vieira777@gmail.com>
2022-12-18 12:42:18 -0300

    Baterias Inclusas - Algumas coisas que vem com o Django

--- Secao_02_Baterias_Inclusas_Algumas_coisas_que_vem_com_o_Django/simplemooc/simplemooc/core/models.py
+++ Secao_02_Baterias_Inclusas_Algumas_coisas_que_vem_com_o_Django/simplemooc/simplemooc/core/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+class CourseManager(models.Manager):
+
+    def search(self, query):
+        return self.get_queryset().filter(models.Q(name__icontains=query) | models.Q(description__icontains=query))
+
+
 class Course(models.Model):
     name = models.CharField('Nome', max_length=100)
     slug = models.SlugField('Atalho')
@@ -9,3 +15,5 @@ class Course(models.Model):
     image = models.ImageField(upload_to='courses/images', verbose_name='Imagem', null=True, blank=True)
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+
+    objects = CourseManager()

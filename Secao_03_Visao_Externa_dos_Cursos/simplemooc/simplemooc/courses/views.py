e5dbb8d2cfd2c02caf9d7c59fa78af550399a4e5
Romulo Vieira <romulo.vieira777@gmail.com>
2023-04-25 20:42:17 -0300

    Visão Externa dos Cursos

--- Secao_03_Visao_Externa_dos_Cursos/simplemooc/simplemooc/courses/views.py
+++ Secao_03_Visao_Externa_dos_Cursos/simplemooc/simplemooc/courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Course
+from .forms import ContactCourse
 
 
 def index(request):
@@ -20,11 +21,18 @@ def index(request):
 #     template_name = 'courses/details.html'
 #     return render(request, template_name, context)
 
-
 def details(request, slug):
     course = get_object_or_404(Course, slug=slug)
-    context = {
-        'course': course
-    }
+    context = {}
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+        if form.is_valid():
+            context['is_valid'] = True
+            form.send_mail(course)
+            form = ContactCourse()
+    else:
+        form = ContactCourse()
+    context['form'] = form
+    context['course'] = course
     template_name = 'courses/details.html'
     return render(request, template_name, context)
